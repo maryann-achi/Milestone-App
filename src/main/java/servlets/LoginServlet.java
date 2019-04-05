247062a7e721727ba3fbd3fde7f6094a8cb2fe27
Lum-neh Angela <lumneh.angela385@gmail.com>
2019-04-05 13:54:23 +0400

    added login functionality and removed the servlet folder

---
+++ src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+      request.getRequestDispatcher("index.jsp").include(request, response);
+
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+
+        if(password.equals("admin123")){
+            out.println("Welcome to your Milestone dashboard, " + name);
+            HttpSession session = request.getSession();
+            session.setAttribute("name", name);
+        }
+        else{
+            out.println("Your username or password is incorrect, please try again!");
+            request.getRequestDispatcher("index.jsp").include(request, response);
+        }
+
+        out.close();
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <body>
-<h2>My Milestone dashboard!!!</h2>
+    <h2>My Milestone dashboard</h2>
+    <a href="login.jsp">Login</a>
+    <a href="">Help</a>
 </body>
 </html>
