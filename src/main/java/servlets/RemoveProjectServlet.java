aba37b049a78620e75403a5736ffdea855f7d487
Lum-neh Angela <lumneh.angela385@gmail.com>
2019-04-24 02:02:38 +0400

    Added H2Milestone and related functions

--- src/main/java/servlets/RemoveProjectServlet.java
+++ src/main/java/servlets/RemoveProjectServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @WebServlet("/RemoveProjectServlet")
