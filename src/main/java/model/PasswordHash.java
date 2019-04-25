62fc509d52421cccd11913d460b37b51d4c2af3f
Lum-neh Angela <lumneh.angela385@gmail.com>
2019-04-25 22:35:30 +0400

    Implemented hashing

---
+++ src/main/java/model/PasswordHash.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class PasswordHash {
+
+    public static String getHashPassword(String passwordToHash, byte[] salt)
+    {
+        String hashedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt);
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            hashedPassword  = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return hashedPassword ;
+    }
+
+    //Add salt
+    public static byte[] getSalt() throws NoSuchAlgorithmException
+    {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+}
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -10,18 +10,21 @@ public class User {
     private final String name;
     private final String email;
     private final String password;
+    private final byte[] salt;
 
-    public User(String name, String email, String password) {
+    public User(String name, String email, String password, byte[] salt) {
         this.name = name;
         this.email = email;
         this.password = password;
+        this.salt = salt;
     }
 
-    public User(int id, String name, String email, String password) {
+    public User(int id, String name, String email, String password, byte[] salt) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.salt = salt;
     }
 
 
@@ -43,5 +46,9 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public byte[] getSalt() {
+        return salt;
     }
 }
